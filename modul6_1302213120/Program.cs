c76156ddc330f2cdeee7dbfcfff7a9d845f67849
PC7-IFLAB <>
2023-03-28 15:39:55 +0700

    first commit

---
+++ modul6_1302213120/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_1302213120
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
